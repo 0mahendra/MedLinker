5de92130dae849c252676c86b9dac095b515af69
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-03-08 07:49:10 -0800

    Add files via upload

---
+++ frontend/src/Auth/LoginDr.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {  FormControl, FormLabel, Input,VStack } from "@chakra-ui/react";
+const LoginDr = ()=>{
+   const[email,setEmail]=useState();
+   const[password,setPassword]=useState();
+     return (
+        <>
+         <VStack spacing={"1px"} color={"white"}>
+        <FormControl id='email' isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+            placeholder="Enter Your email"
+            onChange={(e)=>setEmail(e.target.value)}
+        />
+
+    </FormControl>
+    <FormControl id='password' isRequired>
+        <FormLabel>Password</FormLabel>
+        <Input
+            placeholder="Enter Your password"
+            onChange={(e)=>setPassword(e.target.value)}
+        />
+
+    </FormControl>
+    </VStack>
+    
+
+
+           
+        </>
+     )
+}
+export default LoginDr;
