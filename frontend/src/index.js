3c07a6716693d099efff6fb3a9c9d2613d972971
Mahendra singh <97393308+0mahendra@users.noreply.github.com>
2024-03-09 18:22:54 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,19 @@
-
 import "./index.css"
 import React from 'react';
 import  App from "./App"
 import ReactDom from 'react-dom';
 import {BrowserRouter} from "react-router-dom"; 
 import { ChakraProvider } from '@chakra-ui/react';
-// import ChatProvider from "./Context/ChatProvider";
+import DataProvider from "./context/DataProvider";
 
 
 ReactDom.render(
   <BrowserRouter>
-  {/* <ChatProvider> */}
+  <DataProvider>
   <ChakraProvider> 
         <App/>
   </ChakraProvider>
-  {/* </ChatProvider> */}
+  </DataProvider>
   </BrowserRouter>
   ,
   document.getElementById("root")
