701ef9eb2b5e6cc8be894229388f81f07d26fc68
Mahendra singh <97393308+0mahendra@users.noreply.github.com>
2024-03-09 18:13:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Info from "./Pages/Info";
 import { Route } from "react-router-dom";
 import PRegistration from "./Auth/PRegistration";
+import Testing from "./Testing";
+import DrRegisteration from "./AuthDctr/DrRegistration";
+import Mianp from "./Pages/patPages.js/Mainp";
 const App = ()=>{
 
   return (
@@ -9,7 +12,11 @@ const App = ()=>{
        <div className="App">
         <Route path ="/" component ={Info} exact / >
         <Route path = "/Ptrgtn" component = {PRegistration}/>
+        <Route path  = "/testing" component = {Testing}/>
+        <Route path = "/Dtrgtn" component = {DrRegisteration} />
+        <Route path = "/pHome" component = {Mianp}/>
         
+
 
         
         
