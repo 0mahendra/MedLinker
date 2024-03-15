ce3c6d32283ae578b83e436cdb5b6f4f5622f54c
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-03-15 04:27:16 -0700

    Update Navbarp.js

--- frontend/src/Pages/patPages.js/Navbarp.js
+++ frontend/src/Pages/patPages.js/Navbarp.js
@@ -1,31 +1,49 @@
-
-import React from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
-import { DataState } from "../../context/DataProvider";
+import React, { useEffect, useState } from "react";
+import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text } from "@chakra-ui/react";
+// import { DataState } from "../../context/DataProvider";
 import ProfileModal from "./ProfileModal";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
-import { keyframes } from "@emotion/react";
+
+
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Navbarp = ()=>{
 
+const history =useHistory();
+  
 
-    const {user} = DataState();
+    
+  const user1 = localStorage.getItem("userInfo");
+  const user = JSON.parse(user1);
     console.log(user);
 
-    const neonBorderAnimation = keyframes`
-  0% { border-color: #ff00ff; }
-  25% { border-color: #00ff00; }
-  50% { border-color: #00ffff; }
-  75% { border-color: #ffff00; }
-  100% { border-color: #ff00ff; }
-`;
+  const logoutHandler = ()=>{
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  }
+
+  const Home = ()=>{
+    history.push("/pHome");
+  }
+
+  const communication = ()=>{
+    history.push("/pCommu");
+  }
+  const FindDctr = ()=>{
+    history.push("/pFindDctr");
+  }
+  const reports=()=>{
+    history.push("/Preports");
+  }
+   
    
 
     return (
         <> 
 
           
-          <Box width={"100vw"} display={"flex"} flexDirection={"row"} >
+          <Box  display={"flex"} flexDirection={"row"} >
              {/* starting segemenet  */}
              <Box width={"20%"} height={"10vh"}  display={"flex"} flexDir={"row"} >
               <img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/healthcare-3099180-2578767.png?f=webp&w=256"/> 
@@ -35,21 +53,30 @@ const Navbarp = ()=>{
              {/* middle segmenet  */}
              <Box width={"60%"} height={"10vh"} display={ "flex"} flexDirection={"row"} justifyContent={"center"} alignItems={"center"}>
 
-                <Button variant={"ghost"} _hover={{  borderColor: "blue.500", borderWidth: "2px"       
-      }}> Home</Button>
-                <Button variant={"ghost"} _hover={{  borderColor: "blue.500", borderWidth: "2px"       
-      }} > Communication</Button>
-                <Button variant={"ghost"}  _hover={{  borderColor: "blue.500", borderWidth: "2px"       
-      }}> Report's</Button>
-                <Button variant={"ghost"}  _hover={{  borderColor: "blue.500", borderWidth: "2px"       
-      }} > find Doctor's</Button>
+                <Button  className = {"btn"} variant={"ghost"} width={"auto"} margin={"5px"} onClick={Home} > Home</Button>
+                <Button className = {"btn"} variant={"ghost"}  width={"auto"} margin={"5px"}   onClick={communication} > Communication</Button>
+                <Button  className = {"btn"} variant={"ghost"}  width={"auto"}  margin={"5px"} onClick={reports}    > Report's</Button>
+                <Button className = {"btn"} variant={"ghost"}  width={"auto"}  margin={"5px"}   onClick={FindDctr}> find Doctor's</Button>
              </Box>
               {/* end segmenet */}
-              <Box width={"20%"} height={"10vh"} display={"flex"} flexDirection={"row-reverse"} alignItems={"center"}>
+              {/* <Box width={"20%"} height={"10vh"} display={"flex"} flexDirection={"row-reverse"} alignItems={"center"}>
               
-                 <ProfileModal  user={user}/>
-              </Box>
+                <ProfileModal  user={user}/> 
+              </Box> */}
 
+              <Menu variant={"ghost"}>
+                    <MenuButton margin={"auto"} as={Button} variant={"ghost"}  rightIcon={<ChevronDownIcon/>}>
+                       <Avatar size={"sm"} cursor={"pointer"} name={user.name} src={user.pic}></Avatar>
+                    </MenuButton>
+                    <MenuList variant={"ghost"}  >
+                         <ProfileModal user={user}>
+                         <MenuItem  variant={"ghost"} color={"black"}>My Profile</MenuItem>
+                         </ProfileModal>
+                       
+                        <MenuDivider></MenuDivider>
+                        <MenuItem  variant ={"ghost"}color={"black"} onClick={logoutHandler}>Logout</MenuItem>
+                    </MenuList>
+                </Menu>
              
           </Box>
 
