b1d18056b48dc34fe809ce4413760313e5a9e5ac
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-03-15 04:33:04 -0700

    Create Commu.js

---
+++ frontend/src/Pages/patPages.js/Commu.js
@@ -0,0 +1,15 @@
+import React  from "react";
+import Navbarp from "./Navbarp";
+import SideDrawer from "./SideDrawer";
+
+const  Commu = ()=>{
+   return (
+    <>
+
+       <Navbarp/>
+       <SideDrawer/>
+    </>
+   )
+}
+
+export default Commu;
