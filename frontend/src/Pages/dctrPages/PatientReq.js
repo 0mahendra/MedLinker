aaae90933246ef81bf7ed147b78da22f5582221e
Mahendra singh <97393308+0mahendra@users.noreply.github.com>
2024-04-18 20:09:48 +0530

    Update PatientReq.js

--- frontend/src/Pages/dctrPages/PatientReq.js
+++ frontend/src/Pages/dctrPages/PatientReq.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Footer from "../patPages/Footer";
 
+
 const PatientReq = ()=>{
     const toast =useToast();
 
@@ -64,41 +65,55 @@ const PatientReq = ()=>{
       }
 
 
+const Dropthis = async( patientId )=>{
+  alert("all clear this dropped");
+  
 
-const createNewChat = async (doctorId, patientId) => {
+  try {
+    const response = await axios.post('/api/Med/drop', {
+      
+      patientId,
+      
+      
+    },);
+    console.log(response);
+  }
+  catch(err){
+       alert(err);
+  }
+}
+const createNewChat = async (doctorId ,patientId ,userId) => {
 //   const toast = useToast(); // Use the useToast hook to get the toast function
+
+      if(!doctorId || !patientId || !userId){
+        toast({
+          title:" error occured",
+          status:"pass",
+          duration:5000,
+          isClosable:true,
+          position:"bottom",
+    
+      });
+      return;
+      } 
+      alert("all clear")
 
   try {
     const response = await axios.post('/api/chats/access', {
       doctorId,
       patientId,
-    });
+      userId,
+    },);
+
      console.log("chat created");
-    // toast.success('Chat created', { autoClose: 5000 }); // Display success toast
+    toast.success('Chat created', { autoClose: 5000 }); // Display success toast
     console.log(response);
 
-    toast({
-      title:"chat created",
-      status:"pass",
-      duration:5000,
-      isClosable:true,
-      position:"bottom",
-
-  });
-    // return response.data.chat; // Return the newly created chat object
+    
+    return response.data.chat; // Return the newly created chat object
 
   } catch (error) {
-    console.log('Error creating chat:');
-
-  //   toast({
-  //     title:"chat alread",
-  //     status:"pass",
-  //     duration:5000,
-  //     isClosable:true,
-  //     position:"bottom",
-
-  // });
-   
+    console.log(error);
   }
 };
 
@@ -107,30 +122,33 @@ const createNewChat = async (doctorId, patientId) => {
 
     return (
         <>
-           <Box overflowY={"scroll"} maxHeight={"70vh"}>
-            <Text textAlign={"center"} fontFamily={"work sans"} fontSize={"30px"}> User's request for Online Appointment </Text>
-            <Flex flexWrap="wrap" justifyContent="space-between">
+           <Box   >
+
+            <Flex flexWrap="wrap" justifyContent="space-between" position={"relative"}>
       {user &&
         user.map((item, index) => (
-            <Box height={"40vh"}  width={"90%"} display={"flex"} flexDirection={"row"} key={index} margin={"5%"} marginBottom={"1%"} borderRadius={"20px"} borderWidth={"5px"}>
-                  <Box width={"20%"} height={"100%"} borderRightWidth={"5px"} display={"flex" } flexDirection={"column"} alignItems={"center"}>
+            <Box height={"40vh"} backdropFilter={"blur(6px)"}  width={"90%"} display={"flex"} flexDirection={"row"} key={index} margin={"5%"} marginBottom={"1%"} borderRadius={"20px"} borderWidth={"5px"}>
+                  <Box width={"25%"} height={"100%"} borderRightWidth={"5px"} display={"flex" } flexDirection={"column"} alignItems={"center"}>
                   <Image marginTop={"8px"} borderRadius={"full"} boxSize={"150px"} src={item.pic}  />
-                  <Text> name : {item.nameP} </Text>
-                  <Text> phone Number : {item.phnumber}</Text>
-                  <Text> Email : {item.emailP}</Text>
+                  <Text fontSize={"25px"} color={"white"}> name : {item.nameP} </Text>
+                  <Text color={"white"}> phone Number : {item.phnumber}</Text>
+                  <Text color={"white"}> Email : {item.emailP}</Text>
                   
                    </Box>
                   <Box width={"60%"} height={"100%"}  >
-                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"}> Problem : {item.problem} </Text>
-                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"}> Decies : {item.dctrtype}  </Text> 
-                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"}> deciese time : {item.deciTime} </Text>
-                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"}> severiety : {item.serverity} </Text>
-                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"}> Previous Doctor : {item.prevDctr} </Text></Box>
+                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"} color={"white"}> Problem : {item.problem} </Text>
+                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"} color={"white"}> Disease : {item.dctrtype}  </Text> 
+                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"} color={"white"}> disease duration : {item.deciTime} </Text>
+                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"} color={"white"}> severity : {item.serverity} </Text>
+                  <Text marginLeft={"5%"} fontFamily={"work sans"} fontSize={"25px"} color={"white"}> Previous Doctor : {item.prevDctr} </Text></Box>
                   <Box width={"20%"} height={"100%"}  display={"flex"}  flexDirection ={"column" } justifyContent={"center"} alignItems={"center"}>
                   
-                  <Button className="btn" onClick={()=>{createNewChat(userd1._id,item._id )}}> click here to connect</Button> 
-                  <Button className="btn"> decline the request</Button> 
+                   <button className="btn"  onClick={()=>{
+                    createNewChat(userd1._id,item._id,item.userId )}}> Connect</button>  
+                    <button className="btn"  onClick={()=>{
+                  Dropthis(item._id)}}> Drop</button> 
                   </Box>
+                  
             </Box>
         ))}
     </Flex>
@@ -138,8 +156,7 @@ const createNewChat = async (doctorId, patientId) => {
               
             
            </Box>
-
-           <Footer/>
+           
         </>
     )
 }
