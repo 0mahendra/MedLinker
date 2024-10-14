e3ded31e27984d9cdf58bfa457b5c3ef8535fbdd
Anuj-kumarr <98229406+Anuj-kumarr@users.noreply.github.com>
2024-10-15 01:12:49 +0530

    Update AboutUs.js

--- frontend/src/Pages/patPages/AboutUs.js
+++ frontend/src/Pages/patPages/AboutUs.js
@@ -18,7 +18,7 @@ const AboutUs=()=>{
       <img src="https://res.cloudinary.com/dojpukez0/image/upload/v1710492570/1710492345074_tn8ilb.jpg" height={"50%"} class="pic"/>
       <div class="text">
         <h2> Anuj Kumar</h2>
-        <h5>Front-end Developer & <span>Designer</span></h5>
+        <h5>Back-end Developer & <span>Designer</span></h5>
           <p>Hello guys,Myself AnujKumar crrently in btech 3rd year pursuing elcetrical engineering from MNNIT Allahabad.I am a frontend developer who is passionate about website designing </p>
         <div class="data">
         <a href="#" class="hire">Hire Me</a>
